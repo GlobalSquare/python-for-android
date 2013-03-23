68f2d2becf3e12ee3c4db9e31c4a7c105a2cfbac
Ed Knutson <knutsoned@gmail.com>
2013-03-23 10:12:04 -0500

    added minimal recipe

---
+++ recipes/minimal/recipe.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+VERSION_minimal=0.1
+URL_minimal=
+MD5_minimal=
+DEPS_minimal=(python)
+BUILD_minimal=$BUILD_PATH/minimal/minimal-$VERSION_sdl
+RECIPE_minimal=$RECIPES_PATH/minimal
+
+function prebuild_minimal() {
+	true
+}
+
+function build_minimal() {
+	cd $SRC_PATH/minimal-jni
+
+	push_arm
+	try ndk-build V=1
+	pop_arm
+
+	try cp -a $SRC_PATH/libs/$ARCH/*.so $LIBS_PATH
+}
+
+function postbuild_minimal() {
+	true
+}
