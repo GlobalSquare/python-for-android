67f80b95e400323cf966bca7892c8c15778a686b
Ed Knutson <knutsoned@gmail.com>
2013-03-30 10:55:31 -0500

    upstream JNIEnv changes from tgs-android

--- src/minimal/jni/minimal_main/minimal_main.c
+++ src/minimal/jni/minimal_main/minimal_main.c
@@ -31,7 +31,7 @@ static int isSdcardUsed = 0;
 
 // JNI env stuff from sdl/src/video/android/SDL_androidvideo.c
 // Extremely wicked JNI environment to call Java functions from C code
-static JNIEnv* JavaEnv = NULL;
+static JavaVM* g_vm = NULL;
 static jobject JavaActivity = NULL;
 static jclass JavaActivityClass = NULL;
 static jmethodID JavaCheckPause = NULL;
@@ -40,19 +40,49 @@ static jmethodID JavaWaitForResume = NULL;
 // for pyjnius
 JNIEnv *SDL_ANDROID_GetJNIEnv()
 {
-    return JavaEnv;
+	// don't hold on to JNIEnv or threads will crash it
+	// get the JavaVM from the Android app and use that to get a safe JNIEnv
+	
+	// TODO figure out how to detect threads and only use this if it's needed
+	// otherwise just cache the JNIEnv we use to get the JavaVM
+    __android_log_print(ANDROID_LOG_DEBUG, "libSDL", "START: getting JNI env");
+	// TODO what if the python thread calling this is a daemon?
+	// there is also an AttachCurrentThreadAsDaemon function
+    JNIEnv* env;
+    int getEnvStat = (*g_vm)->GetEnv(g_vm, (void **)&env, JNI_VERSION_1_4);
+    if (getEnvStat == JNI_EDETACHED) {
+        if ((*g_vm)->AttachCurrentThread(g_vm, (void **) &env, NULL) != 0) {
+	        // TODO handle this, means it couldn't attach
+	        __android_log_print(ANDROID_LOG_ERROR, "libSDL", "failed to attach to current thread");
+        } // else we now have a valid JNIEnv for the calling thread
+    } else if (getEnvStat == JNI_OK) {
+        //
+        __android_log_print(ANDROID_LOG_DEBUG, "libSDL", "cool...");
+    } else if (getEnvStat == JNI_EVERSION) {
+        // TODO handle this but should it ever happen with SDK >= 9?
+        __android_log_print(ANDROID_LOG_ERROR, "libSDL", "JNI version error");
+    }
+    
+    __android_log_print(ANDROID_LOG_DEBUG, "libSDL", "about to return JNIEnv");
+	// TODO per http://developer.android.com/training/articles/perf-jni.html "Threads"
+	// FIXME or else nasty memory leak
+	// see also http://pubs.opengroup.org/onlinepubs/009696799/functions/pthread_key_create.html
+    //g_vm->DetachCurrentThread();
+    return env;
 }
 
 int SDL_ANDROID_CheckPause()
 {
     int rv;
     
+    JNIEnv* JavaEnv = SDL_ANDROID_GetJNIEnv();
     rv = (*JavaEnv)->CallIntMethod( JavaEnv, JavaActivityClass, JavaCheckPause );
     return rv;
 }
 
 void SDL_ANDROID_WaitForResume()
 {
+    JNIEnv* JavaEnv = SDL_ANDROID_GetJNIEnv();
     (*JavaEnv)->CallVoidMethod(JavaEnv, JavaActivityClass, JavaWaitForResume);
 }
 
@@ -66,9 +96,12 @@ JAVA_EXPORT_NAME(PythonActivity_nativeInit) ( JNIEnv*  env, jobject thiz )
 	char * argv[] = { "minimal" };
 
 	// from sdl/src/video/android/SDL_androidvideo.c
-	JavaEnv = env;
+	(*env)->GetJavaVM(env, &g_vm);
+	
 	JavaActivity = gref(thiz);
-	JavaActivityClass = (*JavaEnv)->GetObjectClass(JavaEnv, thiz);
+	
+	JavaActivityClass = (*env)->GetObjectClass(env, thiz);
+	
 //	JavaCheckPause = (*JavaEnv)->GetMethodID(JavaEnv, JavaActivityClass, "checkPause", "()I");
 //	JavaWaitForResume = (*JavaEnv)->GetMethodID(JavaEnv, JavaActivityClass, "waitForResume", "()V");
 
